3ab357ec93ba93f894139e75e9808dfdc7ca1b5b
erikaaatan <eatan18@gmail.com>
2021-07-05 13:16:09 -0400

    Make new xcode proj and UIViewControllers for login and register

---
+++ groupLINQ/groupLINQ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  groupLINQ
+//
+//  Created by Erika Tan on 7/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
