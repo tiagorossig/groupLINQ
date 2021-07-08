e70c4236be6e8ff245845d1a17a6117448f8e770
Tiago Grimaldi Rossi <tiagogrimaldirossi@Tiagos-Mac-mini.local>
2021-07-08 10:41:37 -0400

    added base XCode project

---
+++ GroupLINQ/GroupLINQ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GroupLINQ
+//
+//  Created by Tiago Grimaldi Rossi on 7/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
