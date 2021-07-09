afd52e4e5ebd43a8b23f5f98a04c8fcf4032644e
Tiago Grimaldi Rossi <tiagogrimaldirossi@Tiagos-Mac-mini.local>
2021-07-08 20:03:34 -0400

    create class popup

---
+++ GroupLINQ/GroupLINQ/ClassOverviewVC.swift
@@ -0,0 +1,34 @@
+//
+//  ClassOverviewVC.swift
+//  GroupLINQ
+//
+//  Created by Tiago Grimaldi Rossi on 7/8/21.
+//
+
+import UIKit
+
+class ClassOverviewVC: UIViewController {
+    
+    var className = ""
+    var delegate: UIViewController!
+
+    @IBOutlet weak var classNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        classNameLabel.text = className
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GroupLINQ/GroupLINQ/ViewController.swift
+++ GroupLINQ/GroupLINQ/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var className = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func createClassPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Class Name", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.text = ""
+        }
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            self.className = alert.textFields![0].text!
+            self.performSegue(withIdentifier: "createClassSegue", sender: self)
+       
+//            let vc = self.storyboard?.instantiateViewController(identifier: "classOverview")
+//            self.navigationController?.pushViewController(vc!, animated: true)
+//
+        }))
 
-
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "createClassSegue" ,
+            let nextVC = segue.destination as? ClassOverviewVC {
+            nextVC.delegate = self
+            nextVC.className = className
+        }
+    }
+    
 }
 
