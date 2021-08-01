e77687dc46cf51fa8eefd2d0f11c413ae86b5224
asb3773 <anishabagaria2097@gmail.com>
2021-08-01 18:10:19 -0500

    survey

--- GroupLINQ/GroupLINQ/SurveyVC.swift
+++ GroupLINQ/GroupLINQ/SurveyVC.swift
@@ -11,6 +11,11 @@ class SurveyVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        calendarWeekView.setupCalendar(numOfDays: 7,
+                                       setDate: Date(),
+                                       allEvents: JZWeekViewHelper.getIntraEventsByDate(originalEvents: events),
+                                       scrollType: .pageScroll,
+                                       firstDayOfWeek: .Monday)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -22,4 +27,24 @@ class SurveyVC: UIViewController {
             overrideUserInterfaceStyle = .light
         }
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        JZWeekViewHelper.viewTransitionHandler(to: size, weekView: calendarWeekView)
+    }
+    
+    override func registerViewClasses() {
+        super.registerViewClasses()
+
+        // Register CollectionViewCell
+        self.collectionView.register(UINib(nibName: "EventCell", bundle: nil), forCellWithReuseIdentifier: "EventCell")
+
+        // Register DecorationView: must provide corresponding JZDecorationViewKinds
+        self.flowLayout.register(BlackGridLine.self, forDecorationViewOfKind: JZDecorationViewKinds.verticalGridline)
+        self.flowLayout.register(BlackGridLine.self, forDecorationViewOfKind: JZDecorationViewKinds.horizontalGridline)
+
+        // Register SupplementrayView: must override collectionView viewForSupplementaryElementOfKind
+        collectionView.register(RowHeader.self, forSupplementaryViewOfKind: JZSupplementaryViewKinds.rowHeader, withReuseIdentifier: "RowHeader")
+    }
+    
+    
 }
