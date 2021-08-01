779ea70d5a08dd4a282aa25d9c0124e94dd7e33e
Tiago Grimaldi Rossi <tiagogrimaldirossi@Tiagos-Mac-mini.local>
2021-08-01 10:18:31 -0400

    added code for profile pic and classOverview, have to finish manual links

--- GroupLINQ/GroupLINQ/ClassOverviewVC.swift
+++ GroupLINQ/GroupLINQ/ClassOverviewVC.swift
@@ -7,23 +7,44 @@
 
 import UIKit
 
-class ClassOverviewVC: UIViewController {
+public let data = ["Tiago", "Anisha", "Erika", "Bulko"]
+
+let memberCellIdentifier = "MemberCell"
+
+class ClassOverviewVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var classNameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     var className = ""
     var delegate: UIViewController!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         classNameLabel.text = className
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        let userDefaults = UserDefaults.standard
-        if userDefaults.bool(forKey: "darkModeEnabled") {
-            overrideUserInterfaceStyle = .dark
-        }
-        else {
-            overrideUserInterfaceStyle = .light
-        }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: memberCellIdentifier, for: indexPath as IndexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        
+        return indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
--- GroupLINQ/GroupLINQ/SettingsVC.swift
+++ GroupLINQ/GroupLINQ/SettingsVC.swift
@@ -7,36 +7,54 @@
 
 import UIKit
 import Firebase
+import AVFoundation
 
-class SettingsVC: UIViewController {
+extension Notification.Name {
+    static let darkModeEnabled = Notification.Name("darkModeEnabled")
+    static let darkModeDisabled = Notification.Name("darkModeDisabled")
+}
+
+class SettingsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     @IBOutlet weak var darkModeSwitch: UISwitch!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let userDefaults = UserDefaults.standard
-        darkModeSwitch.isOn = userDefaults.bool(forKey: "darkModeEnabled")
+        NotificationCenter.default.addObserver(self, selector: #selector(darkModeEnabled(_:)), name: .darkModeEnabled, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(darkModeDisabled(_:)), name: .darkModeDisabled, object: nil)
+        picker.delegate = self
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        let userDefaults = UserDefaults.standard
-        if userDefaults.bool(forKey: "darkModeEnabled") {
-            overrideUserInterfaceStyle = .dark
-        }
-        else {
-            overrideUserInterfaceStyle = .light
-        }
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: .darkModeEnabled, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .darkModeDisabled, object: nil)
     }
     
     @IBAction func darkModeChanged(_ sender: Any) {
-        let userDefaults = UserDefaults.standard
         if darkModeSwitch.isOn {
-            userDefaults.set(true, forKey: "darkModeEnabled")
-            overrideUserInterfaceStyle = .dark
+            
+//            userDefaults.set(true, forKey: "darkModeEnabled")
+
+            // Post the notification to let all current view controllers that the app has changed to dark mode, and they should theme themselves to reflect this change.
+            NotificationCenter.default.post(name: .darkModeEnabled, object: nil)
         }
         else {
-            userDefaults.set(false, forKey: "darkModeEnabled")
-            overrideUserInterfaceStyle = .light
+//            userDefaults.set(false, forKey: "darkModeEnabled")
+
+            // Post the notification to let all current view controllers that the app has changed to non-dark mode, and they should theme themselves to reflect this change.
+            NotificationCenter.default.post(name: .darkModeDisabled, object: nil)
         }
+    }
+    
+    @objc private func darkModeEnabled(_ notification: Notification) {
+        overrideUserInterfaceStyle = .dark
+    }
+
+    @objc private func darkModeDisabled(_ notification: Notification) {
+        overrideUserInterfaceStyle = .light
     }
     
     @IBAction func logoutPressed(_ sender: Any) {
@@ -56,4 +74,67 @@ class SettingsVC: UIViewController {
 
         (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(loginNavController)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let chosenImage = info[.originalImage] as! UIImage
+        
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = chosenImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+        print("User cancelled")
+
+    }
+    
+    @IBAction func profilePicturePressed(_ sender: Any) {
+        
+        if UIImagePickerController.availableCaptureModes(for: .rear) != nil {
+            
+            // there is a rear camera!
+            switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .notDetermined:
+                AVCaptureDevice.requestAccess(for: .video) {
+                    accessGranted in
+                    guard accessGranted == true else { return }
+                }
+            case .authorized:
+                break
+            default:
+                print("Access denied")
+                return
+            }
+            
+            // We are authorized to use the camera
+            
+            picker.allowsEditing = false
+            picker.sourceType = .camera
+            picker.cameraCaptureMode = .photo
+            
+            present(picker, animated: true, completion: nil)
+            
+        } else {
+        
+            // if no camera is available, pop up an alert
+            
+            let alertVC = UIAlertController(
+                title: "No camera",
+                message: "Sorry, this device has no camera",
+                preferredStyle: .alert
+            )
+            
+            let okAction = UIAlertAction(
+                title: "OK",
+                style:.default,
+                handler: nil
+            )
+            alertVC.addAction(okAction)
+            present(alertVC, animated:true, completion:nil)
+        }
+    
+    }
+    
 }
