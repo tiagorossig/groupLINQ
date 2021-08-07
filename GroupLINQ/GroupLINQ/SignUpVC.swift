8c4f44b1f3433c2f105e57f08d89388001be96d3
erikaaatan <eatan18@gmail.com>
2021-08-07 12:01:02 -0400

    add user to db when creating new account

--- GroupLINQ/GroupLINQ/SignUpVC.swift
+++ GroupLINQ/GroupLINQ/SignUpVC.swift
@@ -10,13 +10,14 @@ import Firebase
 
 class SignUpVC: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func signupPressed(_ sender: Any) {
@@ -27,7 +28,29 @@ class SignUpVC: UIViewController {
         else {
           return
         }
-        Auth.auth().createUser(withEmail: email, password: password, completion: { user, error in  self.performSegue(withIdentifier: "HomeSegueIdentifier", sender: self)})
+        Auth.auth().createUser(withEmail: email, password: password, completion: { user, error in
+            let currentUser = Auth.auth().currentUser
+            if let currentUser = currentUser {
+                let uid = currentUser.uid
+                self.db.collection("users").document(uid).setData([
+                    "name": self.nameTextField.text!,
+                    "email": email,
+                    "phoneNumber": self.phoneNumberTextField.text!,
+                    "availableTimes": []
+                ]) { err in
+                    if let err = err {
+                        print("Error adding document: \(err)")
+                    } else {
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+
+                        // This is to get the SceneDelegate object from your view controller
+                        // then call the change root view controller function to change to main tab bar
+                        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
+                    }
+                }
+            }
+        })
     }
 
 }
