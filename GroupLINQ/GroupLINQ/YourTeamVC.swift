b3046fd11d8d7d4c051b97207f5cb07c856d402f
asb3773 <anishabagaria2097@gmail.com>
2021-08-09 16:17:12 -0500

    a few links

---
+++ GroupLINQ/GroupLINQ/YourTeamVC.swift
@@ -0,0 +1,29 @@
+//
+//  YourTeamVC.swift
+//  GroupLINQ
+//
+//  Created by Bagaria on 8/9/21.
+//
+
+import UIKit
+
+class YourTeamVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
